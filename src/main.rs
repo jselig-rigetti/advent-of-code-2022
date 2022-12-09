8d246a69e9a0c4cbec0779a2f351f090f53cb31d
Jake Selig <jselig@rigetti.com>
2022-12-08 17:49:59 -0700

    suppress 'unused' warnings by un-modulizing prior days

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use std::{env, fs};
 
 use miette::Result;
 
-mod day_01;
-mod day_02;
-mod day_03;
-mod day_04;
-mod day_05;
-mod day_06;
-mod day_07;
+// mod day_01;
+// mod day_02;
+// mod day_03;
+// mod day_04;
+// mod day_05;
+// mod day_06;
+// mod day_07;
 mod day_08;
 
 #[tokio::main]
